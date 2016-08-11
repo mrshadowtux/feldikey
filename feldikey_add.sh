bfe238ffa1e19ecf547109c66bc988f39a7f8dc0
mrshadowtux <alexander.feld@farnox.de>
2016-08-12 00:00:19 +0200

    FIXED: typo that prevented this script from working at all

--- feldikey_add.sh
+++ feldikey_add.sh
@@ -3,7 +3,7 @@
 # usage: feldikey_add.sh welcometext _welcome_ "Willkommen bei feldikey, bla bla bla"
 
 title=${1}
-shortcut=${1}
+shortcut=${2}
 text=${3}
 
 echo 'insert into entries(title, shortcut, content) values("'${title}'", "'${shortcut}'", "'${text}'");' | sqlite3 ~/.config/feldikey/feldikey.db
