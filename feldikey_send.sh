bce9c5389a2c7149862bcaa5a1795e16606a7381
mrshadowtux <alexander.feld@farnox.de>
2016-08-12 00:35:25 +0200

    matched to coding guidelines and added explanations

--- feldikey_send.sh
+++ feldikey_send.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 # feldikey_send: Part of feldikey to send out text to the brower
-quicktext=$*
-for quickstep in $(echo $quicktext)
+
+quicktext=${*}
+for quickstep in $(echo ${quicktext})
 do
+	# Each quickstep is a word that will get "customized" by sed to our needs
 	# TODO: Newlines and a lot of custom characters, german umlauts etc
-	quickstep=$(echo $quickstep | sed 's/./& /g')
-	quickstep=$(echo $quickstep | sed 's/ $//g')
-	quickstep=$(echo $quickstep | sed 's/,/comma/g')
-	quickstep=$(echo $quickstep | sed 's/\./period/g')
-	quickstep=$(echo $quickstep | sed 's/ö/odiaeresis/g')
+	quickstep=$(echo ${quickstep} | sed 's/./& /g')
+	quickstep=$(echo ${quickstep} | sed 's/ $//g')
+	quickstep=$(echo ${quickstep} | sed 's/,/comma/g')
+	quickstep=$(echo ${quickstep} | sed 's/\./period/g')
+	quickstep=$(echo ${quickstep} | sed 's/ö/odiaeresis/g')
 	quicktext_final="${quicktext_final} space ${quickstep}"
 done
 
 quicktext_final="${quicktext_final} Return"
 
+# Just for development we actively select the Leafpad window.
+# We won't use that in the end. In production, we will just
+# use xdotool key and directly write into the active window.
 window=$(xdotool search "Unbenannt" | head -1)
 xdotool windowactivate --sync ${window}
 xdotool key ${quicktext_final}
